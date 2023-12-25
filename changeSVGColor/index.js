ed96da8557281cd80d3ddb2a75c1c1dcb874d0f1
徐杰 <coder_xujie@163.com>
2023-12-25 11:28:43 +0800

    12-25

--- changeSVGColor/index.js
+++ changeSVGColor/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+
+const svgStr = fs.readFileSync('./static/config.svg').toString();
+// console.log(svgStr)
+
+const svgChangeColor = svgStr.replace(/#[a-fA-F0-9]{6}/g, '#ff0000')
+// console.log(svgChangeColor)
+
+const svBase64 = Buffer.from(svgChangeColor).toString('base64')
+// console.log(svBase64);
+
+
+
+fs.writeFileSync('./static/configAAA.svg', Buffer.from(svBase64, 'base64'));
 
